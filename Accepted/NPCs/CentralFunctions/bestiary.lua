dbc78b8512964966ecb6a4c23de9fffd3e8e0b69
Materia <anthony.perez@gmail.com>
2022-02-08 21:29:35 -0600

    bestiary2

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -1,14 +1,13 @@
 bestiary = {
 	click = function(player, npc)
 		monster_id = player:input("What Monster Graphic would you like to see?")
-		actual_monster_id = tonumber(monster_id, 10)
-		if (actual_monster_id >= 0) then
-			local t = {graphic = convertGraphic(actual_monster_id, "monster"), color = 0}
+		if (tonumber(monster_id, 10) >= 0) then
+			local t = {graphic = convertGraphic(toNumber(monster_id,10), "monster"), color = 0}
 			player.npcGraphic = t.graphic
 			player.npcColor = t.color
 			player.dialogType = 0
 		else
-			npc:talk(1, "Get the fuck out of here you piece of shit. Thats not a number.")
+			npc:talk(1, "Get the fuck out of here you piece of shit. Thats not a number")
 		end
 	end
 }
