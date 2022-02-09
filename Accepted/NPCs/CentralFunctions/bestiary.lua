916c337e75e90e7c29ed09971677c08ba99aa947
Materia <anthony.perez@gmail.com>
2022-02-08 21:36:04 -0600

    adjust

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -1,12 +1,13 @@
 bestiary = {
 	click = function(player, npc)
 		monsterId = player:inputNumberCheck(player:input("What Monster Graphic would you like to see?"))
-		local t = {graphic = convertGraphic(monster_id, "monster"), color = 0}
-		player.npcGraphic = t.graphic
-		player.npcColor = t.color
-		player.dialogType = 2
-		local opts = {"Next >>", "Options", "<< Previous"}
-		menu = player:menuSeq("Make your choice", opts, {})
-		
+		if (monsterId >= 0) then
+			local t = {graphic = convertGraphic(monster_id, "monster"), color = 0}
+			player.npcGraphic = t.graphic
+			player.npcColor = t.color
+			player.dialogType = 2
+			local opts = {"Next >>", "Options", "<< Previous"}
+			menu = player:menuSeq("Make your choice", opts, {})
+		end
 	end
 }
