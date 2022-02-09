d92a65be368f7777031f04da3cc43b477bd0fac9
Materia <anthony.perez@gmail.com>
2022-02-08 22:01:26 -0600

    blah

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -6,16 +6,15 @@ bestiary = {
 				local t = {graphic = convertGraphic(monsterId, "monster"), color = 0}
 				player.npcGraphic = t.graphic
 				player.npcColor = t.color
-				player.dialogType = 2
+				player.dialogType = 0
 				player.registry["beastiary"] = monsterId
 				player:sendMinitext("Set monster# " .. monsterId)
 				beastiary.browse_monster(player,npc)
 			end
 		end),
     browse_monster = function(player, npc)
-		local name = "<b>[" .. npc.name .. "]\n\n"
 		graphic = player.registry["beastiary"]
-		player:sendMinitext("Made it to browse")
+		player:sendMinitext("Made it to browse ".. graphic)
 		local t = {graphic = convertGraphic(graphic, "monster"), color = npc.lookColor}
 		player.npcGraphic = t.graphic
 		player.npcColor = t.color
@@ -26,12 +25,13 @@ bestiary = {
 		table.insert(opts, "Next ->>")
 		table.insert(opts, "<<- Previous")
 
-		menu = player:menuSeq(name .. "\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
+		menu = player:menuSeq("\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
 		if menu == "Next ->>" then
 			player.registry["beastiary"] = player.registry["beastiary"] + 1
+			return beastiary.browse_monster(player,npc)
 		elseif menu == "<<- Previous" then
 			player.registry["beastiary"] = player.registry["beastiary"] - 1
+			return beastiary.browse_monster(player,npc)
 		end
-		return beastiary.browse_monster(player,npc)
 	end
 }
