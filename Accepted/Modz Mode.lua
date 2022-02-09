62a6fda41813445e7babd8fc3b8078ec1031631d
Materia <anthony.perez@gmail.com>
2022-02-08 23:01:43 -0600

    modz mode

---
+++ Accepted/Modz Mode.lua
@@ -0,0 +1,41 @@
+modz_mode = {
+	cast = function(player)
+		disguise = player:inputNumberCheck(player:input("Pick a number?"))
+
+		local duration = 60000
+		local anim = 249
+		local sound = 36
+
+		if player:hasDuration("modz_mode") then
+			player:setDuration("modz_mode", 0)
+			player.gfxClone = 0
+			player:updateState()
+			return
+		else
+			if player.state == 0 then
+				player.disguise = disguise
+				player.disguiseColor = 0
+				player.state = 4
+				player:updateState()
+				player:sendAnimation(anim)
+				player:playSound(sound)
+				player:sendMinitext("You go into Modz Mode!")
+				player:setDuration("modz_mode", duration)
+			end
+		end
+	end,
+	uncast = function(player)
+		local anim = 249
+		local sound = 36
+
+		if player.state == 4 then
+			player.disguise = 0
+			player.disguiseColor = 0
+			player.state = 0
+			player:updateState()
+			player:sendAnimation(anim)
+			player:playSound(sound)
+			player:sendMinitext("Your Spiritual Transformation ends")
+		end
+	end
+}
