89483e2b2b7264f4a0766fd45d7b657d425d49e6
Materia <anthony.perez@gmail.com>
2022-02-08 21:58:40 -0600

    menu

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -15,16 +15,18 @@ bestiary = {
     browse_monster = function(player, npc)
 		local name = "<b>[" .. npc.name .. "]\n\n"
 		graphic = player.registry["beastiary"]
+		player:sendMinitext("Made it to browse")
 		local t = {graphic = convertGraphic(graphic, "monster"), color = npc.lookColor}
 		player.npcGraphic = t.graphic
 		player.npcColor = t.color
 		player.dialogType = 0
 		local stop = 0
 		local opts = {}
+		player:sendMinitext("Made it to opts")
 		table.insert(opts, "Next ->>")
 		table.insert(opts, "<<- Previous")
 
-		player:menuSeq(name .. "\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
+		menu = player:menuSeq(name .. "\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
 		if menu == "Next ->>" then
 			player.registry["beastiary"] = player.registry["beastiary"] + 1
 		elseif menu == "<<- Previous" then
