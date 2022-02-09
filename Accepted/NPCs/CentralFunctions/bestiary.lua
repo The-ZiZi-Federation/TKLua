2a3021936d9da0db404ad7f1163a0a686c1a4cb9
Materia <anthony.perez@gmail.com>
2022-02-08 21:56:43 -0600

    test

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -1,14 +1,14 @@
 bestiary = {
 	click = async(
 		function(player, npc)
-			monsterId = player:inputNumberCheck(player:input("What Monster Graphic would you like to see?"))
+			monsterId = math.abs(tonumber(math.floor(player:input("What Monster Graphic would you like to see?"))))
 			if (monsterId >= 0) then
 				local t = {graphic = convertGraphic(monsterId, "monster"), color = 0}
 				player.npcGraphic = t.graphic
 				player.npcColor = t.color
 				player.dialogType = 2
 				player.registry["beastiary"] = monsterId
-				player:sendMinitext("Set monster#" .. monsterId)
+				player:sendMinitext("Set monster# " .. monsterId)
 				beastiary.browse_monster(player,npc)
 			end
 		end),
@@ -24,11 +24,12 @@ bestiary = {
 		table.insert(opts, "Next ->>")
 		table.insert(opts, "<<- Previous")
 
-		player:menuString(name .. "\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
+		player:menuSeq(name .. "\nThat was Monster #" .. graphic .. ", you like that shit?", opts)
 		if menu == "Next ->>" then
 			player.registry["beastiary"] = player.registry["beastiary"] + 1
 		elseif menu == "<<- Previous" then
 			player.registry["beastiary"] = player.registry["beastiary"] - 1
 		end
+		return beastiary.browse_monster(player,npc)
 	end
 }
