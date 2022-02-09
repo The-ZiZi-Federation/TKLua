ca9753a7cc730c73ed4c8a26aa4c01e987a47356
Materia <anthony.perez@gmail.com>
2022-02-08 21:37:01 -0600

    monsterId

--- Accepted/NPCs/CentralFunctions/bestiary.lua
+++ Accepted/NPCs/CentralFunctions/bestiary.lua
@@ -2,7 +2,7 @@ bestiary = {
 	click = function(player, npc)
 		monsterId = player:inputNumberCheck(player:input("What Monster Graphic would you like to see?"))
 		if (monsterId >= 0) then
-			local t = {graphic = convertGraphic(monster_id, "monster"), color = 0}
+			local t = {graphic = convertGraphic(monsterId, "monster"), color = 0}
 			player.npcGraphic = t.graphic
 			player.npcColor = t.color
 			player.dialogType = 2
