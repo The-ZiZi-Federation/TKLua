78f351d89deb63118902098657fe6517e84c16ac
Materia <anthony.perez@gmail.com>
2022-02-08 23:13:23 -0600

    talk

--- Accepted/Modz Mode.lua
+++ Accepted/Modz Mode.lua
@@ -2,10 +2,10 @@ modz_mode = {
 	cast = function(player)
 		local disguise = player:inputNumberCheck(player:input("Pick a number."))
 
+		player:talk(2, "You go into Modz Mode! " ..disguise)
 		local duration = 60000
 		local anim = 249
 		local sound = 36
-		player:sendMinitext("You go into Modz Mode! " ..disguise)
 		if player:hasDuration("modz_mode") or player.state == 4 then
 			player:setDuration("modz_mode", 0)
 			player.disguise = 0
